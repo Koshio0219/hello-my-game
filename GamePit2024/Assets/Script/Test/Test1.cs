899910ed4746cb94bb689cdd553b66a58a9e1e94
koshio <koshio0219@gmail.com>
2023-11-03 00:09:23 +0900

    add GameData.cs and GameManager.cs class

--- GamePit2024/Assets/Script/Test/Test1.cs
+++ GamePit2024/Assets/Script/Test/Test1.cs
@@ -3,21 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class Test1 : MonoBehaviour
+namespace Game.Test
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Test1 : MonoBehaviour
     {
-        gameObject.transform.DOMoveX(transform.position.x + 5, 1.0f).SetEase(Ease.InCubic).OnComplete(() =>
+        void Start()
         {
-            Debug.Log($"the game object {gameObject.name} has move to its x+5 position.");
-        });
- 
-    }
+            gameObject.transform.DOMoveX(transform.position.x + 5, 1.0f).SetEase(Ease.InCubic).OnComplete(() =>
+            {
+                Debug.Log($"the game object {gameObject.name} has move to its x+5 position.");
+            });
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
     }
 }
