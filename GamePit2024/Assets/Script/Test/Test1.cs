3e4dc7b21d13f406ed8e4da17a4b6de1f68793a7
koshio <koshio0219@gmail.com>
2023-11-05 17:16:18 +0900

    add SceneLoader(Async) and canvas fix Resolution class

--- GamePit2024/Assets/Script/Test/Test1.cs
+++ GamePit2024/Assets/Script/Test/Test1.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
+using Game.Loader;
 
 namespace Game.Test
 {
     public class Test1 : MonoBehaviour
     {
+        public Button loadScene;
+
         void Start()
         {
             gameObject.transform.DOMoveX(transform.position.x + 5, 1.0f).SetEase(Ease.InCubic).OnComplete(() =>
@@ -14,6 +18,18 @@ namespace Game.Test
                 Debug.Log($"the game object {gameObject.name} has move to its x+5 position.");
             });
 
+            CheckLoad();
+        }
+
+        private void CheckLoad()
+        {
+            if (loadScene == null) return;
+
+            loadScene.onClick.AddListener(() =>
+            {
+                SceneLoader.Instance.OnClickLoadScene("Stage");
+                loadScene.onClick.RemoveAllListeners();
+            });
         }
     }
 }
