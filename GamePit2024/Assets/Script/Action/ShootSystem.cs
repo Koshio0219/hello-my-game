ac4742e06e92b1b6d2619f1b1a584ae8174a4557
koshio <koshio0219@gmail.com>
2023-11-30 01:18:29 +0900

    create the Bullet.cs

--- GamePit2024/Assets/Script/Action/ShootSystem.cs
+++ GamePit2024/Assets/Script/Action/ShootSystem.cs
@@ -1,20 +1,48 @@
-﻿using Game.Base;
+﻿using Cysharp.Threading.Tasks;
+using Game.Base;
 using Game.Data;
+using Game.Loader;
+using Game.Unit;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 namespace Game.Action
 {
     public class ShootSystem : FireBase
     {
-        public override void FireBegin(int creatorId = 0, AttackState attackState = AttackState.Normal)
-        {
+        public AssetReference bulletPrefab;
+        public Transform firePos;
+        public GameObject target;
 
+        public override async void FireBegin(int creatorId = 0, AttackState attackState = AttackState.Normal)
+        {
+            //test
+            var bullet = await AssetLoader.Instance.Load<Bullet>(bulletPrefab, this.GetCancellationTokenOnDestroy());
+            bullet.transform.SetParent(null);
+            bullet.transform.position = firePos.position;
+            bullet.transform.forward = firePos.forward;
+            bullet.Init(target);
         }
 
         public override void FireShut()
         {
 
+        }
+
+        public override void SetFirePos(Transform pos)
+        {
+            firePos = pos;
+        }
+
+        public override void SetTarget(GameObject tar)
+        {
+            target = tar;
+        }
+
+        public override void SetDamage(float damage, float damageUpSpeed = 0, int maxDamage = 0, float exCritRate = 0)
+        {
+            
         }
     }
 }
---
+++ GamePit2024/Assets/Script/Unit/Bullet.cs
@@ -0,0 +1,87 @@
+﻿using Cysharp.Threading.Tasks;
+using Game.Base;
+using Game.Framework;
+using System.Collections;
+using System.Threading;
+using UnityEngine;
+
+namespace Game.Unit
+{
+    [System.Serializable]
+    public class BulletProp
+    {
+        public float speed;
+
+        public float lifeTime = 3f;
+
+        public float angleSpeed = 120f;
+
+        public float acceleration = 30f;
+
+        public float maxSpeed = 600f;
+    }
+
+    public class Bullet : MonoBehaviour
+    {
+        public BulletProp prop;
+        public GameObject Target { get;private set; }
+
+        private CancellationToken token = CancellationToken.None;
+
+        private System.Action recycleAction = null;
+        private System.Action moveAction = null;
+
+        public void Init(GameObject target)
+        {
+            Target = target;
+            token = this.GetCancellationTokenOnDestroy();
+            InitAction();
+            recycleAction.Invoke();
+            moveAction.Invoke();
+        }
+
+        private void InitAction()
+        {
+             recycleAction = UniTask.Action(async (_) =>
+             {
+                 await UniTask.Delay((int)(prop.lifeTime * 1000));
+                 GameObjectPool.Instance.RecycleObj(gameObject);
+             }, token);
+            moveAction = UniTask.Action(async (_) =>
+            {
+                while (isActiveAndEnabled)
+                {
+                    Move();
+                    await UniTask.DelayFrame(1, PlayerLoopTiming.FixedUpdate);
+                }
+            }, token);
+        }
+
+        private void Move()
+        {
+            float deltaTime = Time.fixedDeltaTime;
+            if (Target != null && prop.angleSpeed > 0)
+            {
+                Vector3 offset = (Target.transform.position - transform.position).normalized;
+
+                float angle = Vector3.Angle(transform.forward, offset);
+
+                float needTime = angle * 1.0f / prop.angleSpeed;
+
+                transform.forward = Vector3.Lerp(transform.forward, offset, deltaTime / needTime).normalized;
+            }
+            if (prop.speed < prop.maxSpeed)
+            {
+                prop.speed += deltaTime * prop.acceleration;
+            }
+            transform.position += transform.forward * prop.speed * deltaTime;
+        }
+
+        private void OnDisable()
+        {
+            recycleAction = null;
+            moveAction = null;
+            token = CancellationToken.None;
+        }
+    }
+}
