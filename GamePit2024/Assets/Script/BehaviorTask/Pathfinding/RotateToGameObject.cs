d8969d55046a270ece3ad76a54dd677d070370d5
koshio <koshio0219@gmail.com>
2023-11-29 15:56:57 +0900

    create the BehaviorTask/Pathfinding/RotateToGameObject.cs

---
+++ GamePit2024/Assets/Script/BehaviorTask/Pathfinding/RotateToGameObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+using BehaviorDesigner.Runtime.Tasks;
+using Unity.Services.Analytics.Internal;
+using BehaviorDesigner.Runtime;
+using UnityEngine.AI;
+
+namespace Game.BehaviorTask
+{
+    public class RotateToGameObject : BehaviorDesigner.Runtime.Tasks.Action
+    {
+        [RequiredField]
+        public SharedGameObject target;
+        public SharedFloat speed = 2;
+        public SharedFloat angleDifference = 5;
+        public SharedVector3 upVector = Vector3.up;
+        public bool waitActionFinish;
+
+        public override TaskStatus OnUpdate()
+        {
+            if (target.Value == null) return TaskStatus.Failure;
+
+            var dir = target.Value.transform.position - transform.position;
+            if (Vector3.Angle(dir, transform.forward) <= angleDifference.Value)
+            {
+                return TaskStatus.Success;
+            }
+         
+            transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, dir, speed.Value * Time.deltaTime, 0), upVector.Value);
+            if (!waitActionFinish)
+            {
+                return TaskStatus.Success;
+            }
+
+            return TaskStatus.Running;
+        }
+
+    }
+}
