0df38191ab9ef985674d18343da0746c337abb76
koshio <koshio0219@gmail.com>
2023-12-13 23:33:47 +0900

    add the SimpleBillboard.cs and make the EnemyHpBar face to camera

---
+++ GamePit2024/Assets/Script/Action/SimpleBillboard.cs
@@ -0,0 +1,22 @@
+﻿using Cysharp.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//make the obj face to camera
+public class SimpleBillboard : MonoBehaviour
+{
+    public PlayerLoopTiming playerLoopTiming = PlayerLoopTiming.Update;
+    private void Awake()
+    {
+        var main = Camera.main;
+        UniTask.Void(async (_) =>
+        {
+            while (isActiveAndEnabled && !_.IsCancellationRequested)
+            {
+                transform.forward = main.transform.forward;
+                await UniTask.DelayFrame(1, playerLoopTiming, this.GetCancellationTokenOnDestroy());
+            }
+        },this.GetCancellationTokenOnDestroy());
+    }
+}
