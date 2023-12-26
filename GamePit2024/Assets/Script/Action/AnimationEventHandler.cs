39fe0ecc0badc9b1da3bc04054b497665631c4f4
koshio <koshio0219@gmail.com>
2023-12-26 18:16:50 +0900

    Slime AnimationEventHandler

---
+++ GamePit2024/Assets/Script/Action/AnimationEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Action
+{
+    public class AnimationEventHandler : MonoBehaviour
+    {
+        // slime animation
+        public void AlertObservers(string message)
+        {
+
+            if (message.Equals("AnimationDamageEnded"))
+            {
+                //Debug.Log("DamageAnimationEnded");
+            }
+
+            if (message.Equals("AnimationAttackEnded"))
+            {
+
+            }
+
+            if (message.Equals("AnimationJumpEnded"))
+            {
+
+            }
+        }
+    }
+}
+
--- GamePit2024/Assets/Script/Unit/Slime.cs
+++ GamePit2024/Assets/Script/Unit/Slime.cs
@@ -81,25 +81,5 @@ namespace Game.Unit
             SetFace(faces.damageFace);
             SetSpeed();
         }
-
-        // Animation Event
-        public void AlertObservers(string message)
-        {
-
-            if (message.Equals("AnimationDamageEnded"))
-            {
-                //Debug.Log("DamageAnimationEnded");
-            }
-
-            if (message.Equals("AnimationAttackEnded"))
-            {
-
-            }
-
-            if (message.Equals("AnimationJumpEnded"))
-            {
-
-            }
-        }
     }
 }
