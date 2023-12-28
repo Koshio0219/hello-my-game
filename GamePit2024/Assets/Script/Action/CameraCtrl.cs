e6df554b8e32f917c9720a3b281113145d4b136d
koshio <koshio0219@gmail.com>
2023-12-28 23:17:44 +0900

    add CameraCtrl.cs and fix up the ScrollerSender.cs

---
+++ GamePit2024/Assets/Script/Action/CameraCtrl.cs
@@ -0,0 +1,51 @@
+﻿using Cysharp.Threading.Tasks;
+using Cysharp.Threading.Tasks.Linq;
+using Game.Data;
+using Game.Framework;
+using Game.Manager;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Game.Action
+{
+    public class CameraCtrl : MonoBehaviour
+    {
+        public float speed = 1f;
+        private int inputIdx;
+        private bool inputable = false;
+
+        async void Awake()
+        {
+            var data = await GameData.Instance.GetPlayerParameter();
+            inputIdx = data.GamepadNumber_D;
+            inputable = false;
+            EventQueueSystem.AddListener<StageStatesEvent>(StageStatesHandler);
+        }
+
+        private void OnDestroy()
+        {
+            EventQueueSystem.RemoveListener<StageStatesEvent>(StageStatesHandler);
+        }
+
+        private void StageStatesHandler(StageStatesEvent e)
+        {
+            if (e.to == StageStates.BattleStarted) 
+                inputable = true;
+            if (e.to == StageStates.BattleClear || e.to == StageStates.GameOver)
+                inputable = false;
+        }
+
+        private void LateUpdate()
+        {
+            if (!inputable) return;
+            if (Gamepad.all.Count > 0 && inputIdx >= 0 && Gamepad.all[inputIdx] != null)
+            {
+                var input = Gamepad.all[inputIdx].rightStick.ReadValue().x;
+                transform.Translate(new Vector3(input * speed * Time.deltaTime, 0, 0));
+            }
+        }
+    }
+}
+
--- GamePit2024/Assets/Script/Action/ScrollerSender.cs
+++ GamePit2024/Assets/Script/Action/ScrollerSender.cs
@@ -1,5 +1,6 @@
 ﻿using Cysharp.Threading.Tasks;
 using Cysharp.Threading.Tasks.Linq;
+using Game.Base;
 using Game.Data;
 using Game.Framework;
 using Game.Manager;
@@ -10,38 +11,31 @@ using UnityEngine.InputSystem;
 
 namespace Game.Action
 {
-    public class ScrollerSender : MonoBehaviour
+    public class ScrollerSender : MonoBehaviour,IInit
     {
-        public AsyncReactiveProperty<float> posX;
+        [SerializeField] private Transform target;
+        private AsyncReactiveProperty<float> targetPosx;
+        private float lastPosx;
+
         public float speed = 1f;
         private Camera mainCamera;
+        private System.Action updateAction = null;
+        private System.Action initAction = null;
 
         private void Start()
         {
             mainCamera = Camera.main;
+            if (target == null) target = transform;
+            initAction = Init;
 
-            posX.Value = transform.position.x;
-
-            posX.ForEachAsync(x =>
+            updateAction = UniTask.Action(async (_) =>
             {
-                Debug.Log($"current posX is changed ,value is :{x}");
-
-            }, this.GetCancellationTokenOnDestroy()).Forget();
-
-
-            var temp = UniTask.Action(async (_) =>
-            {
-                while (true)
+                while (this && isActiveAndEnabled)
                 {
-                    //...//
                     UpdateAction();
                     await UniTask.DelayFrame(1, PlayerLoopTiming.Update);
                 }
-
-            }, this.GetCancellationTokenOnDestroy());
-            temp.Invoke();
-
-            
+            }, this.GetCancellationTokenOnDestroy());       
         }
 
         private void Awake()
@@ -57,45 +51,68 @@ namespace Game.Action
         private void StageStatesHandler(StageStatesEvent e)
         {
             if (e.to != StageStates.BattleStarted) return;
-
-            //var temp = UniTask.UnityAction(async (_) =>
-            //{
-            //    while (true)
-            //    {
-            //        //...//
-            //       await UniTask.DelayFrame(1, PlayerLoopTiming.Update);
-            //    }
-
-            //},this.GetCancellationTokenOnDestroy());
+            initAction?.Invoke();
+            updateAction?.Invoke();
         }
 
-        void UpdateAction()
+        private void UpdateAction()
         {
-            var dt = Time.deltaTime;
-            if (Input.GetKey(KeyCode.D))
-            {
-                var dis = dt * speed;
-                transform.Translate(new Vector3(dis, 0, 0));
-                //if (renderer1.IsVisableInCamera())
-                if (mainCamera.IsVisableInCamera(transform.position, 0.1f))
-                {
-                    //posX.Value = transform.position.x;
-                    EventQueueSystem.QueueEvent(new BackgroundScrollerEvent(-dis));
-                }
-                return;
-            }
-
-            if (Input.GetKey(KeyCode.A))
-            {
-                var dis = dt * speed;
-                transform.Translate(new Vector3(-dt * speed, 0, 0));
-                //if (renderer1.IsVisableInCamera())
-                if (mainCamera.IsVisableInCamera(transform.position, 0.1f))
-                {
-                    //posX.Value = transform.position.x;
-                    EventQueueSystem.QueueEvent(new BackgroundScrollerEvent(dis));
-                }
-            }
+            targetPosx.Value = target.position.x;
         }
+
+        private void PosxChangeHandler(float posx)
+        {
+            if (!mainCamera.IsVisableInCamera(target.transform.position, 0.1f))
+                return;
+
+            var dis = posx - lastPosx;
+            EventQueueSystem.QueueEvent(new BackgroundScrollerEvent(-dis * speed));     
+        }
+
+        public void Init()
+        {
+            lastPosx = transform.position.x;
+            targetPosx = new AsyncReactiveProperty<float>(lastPosx);
+            targetPosx.ForEachAsync(x =>
+            {
+                Debug.Log($"target:{gameObject.name} current posx is changed ,value is :{x}");
+                if (lastPosx == x) return;
+                PosxChangeHandler(x);
+                lastPosx = x;
+
+            }, this.GetCancellationTokenOnDestroy()).Forget();
+            initAction = null;
+        }
+
+        #region test
+        //void UpdateAction()
+        //{
+        //    var dt = Time.deltaTime;
+        //    if (Input.GetKey(KeyCode.D))
+        //    {
+        //        var dis = dt * speed;
+        //        transform.Translate(new Vector3(dis, 0, 0));
+        //        //if (renderer1.IsVisableInCamera())
+        //        if (mainCamera.IsVisableInCamera(transform.position, 0.1f))
+        //        {
+        //            //posX.Value = transform.position.x;
+        //            EventQueueSystem.QueueEvent(new BackgroundScrollerEvent(-dis));
+        //        }
+        //        return;
+        //    }
+
+        //    if (Input.GetKey(KeyCode.A))
+        //    {
+        //        var dis = dt * speed;
+        //        transform.Translate(new Vector3(-dt * speed, 0, 0));
+        //        //if (renderer1.IsVisableInCamera())
+        //        if (mainCamera.IsVisableInCamera(transform.position, 0.1f))
+        //        {
+        //            //posX.Value = transform.position.x;
+        //            EventQueueSystem.QueueEvent(new BackgroundScrollerEvent(dis));
+        //        }
+        //    }
+        //}
+        #endregion
     }
 }
