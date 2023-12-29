4391cf0c8445409bec9ed3f2ce37d98a1c9a3dc3
Matsu <20fi101@ms.dendai.ac.jp>
2023-12-29 19:50:38 +0900

    Add StartSceneUI

---
+++ GamePit2024/Assets/Matsunaga/Script/Utility/TitleSelectUI.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine;
+using System;
+
+public class TitleSelectUI : MonoBehaviour
+{
+    public enum PlayerType
+    {
+        First = 0,
+        Second = 1,
+        Third = 2,
+    }
+    public enum StateEnum
+    {
+        None = -1,
+        Start,
+        Setting,
+        HomePage,
+        Exit,
+    }
+    [SerializeField] private PlayerType _PlayerType;
+    [SerializeField] private List<GameObject> _ParList = new List<GameObject>();
+    private StateEnum _State = StateEnum.Start;
+    public StateEnum DecideState = StateEnum.None;
+    void Start()
+    {
+        ChangeState(StateEnum.Start);
+    }
+    void Update()
+    {
+        var playerNum = (int)_PlayerType;
+        // 該当のゲームパッドが接続されていないと動かない
+        if (Gamepad.all.Count < playerNum + 1) return;
+        // 何も選んでいない時
+        if (DecideState == StateEnum.None)
+        {
+            // 上を押すとはじめるを選択
+            if (Gamepad.all[playerNum].dpad.up.wasPressedThisFrame)
+            {
+                ChangeState((StateEnum)Enum.ToObject(typeof(StateEnum), ((int)_State + 3) % 4));
+            }
+            // 下を押すとチュートリアルを選択
+            if (Gamepad.all[playerNum].dpad.down.wasPressedThisFrame)
+            {
+                ChangeState((StateEnum)Enum.ToObject(typeof(StateEnum), ((int)_State + 1) % 4));
+            }
+            /*// 上を押すとはじめるを選択
+            if (Gamepad.all[playerNum].dpad.up.wasPressedThisFrame)
+            {
+                ChangeState(StateEnum.Start);
+            }
+            // 下を押すとチュートリアルを選択
+            if (Gamepad.all[playerNum].dpad.down.wasPressedThisFrame)
+            {
+                ChangeState(StateEnum.Tutorial);
+            }*/
+            // ○を押すと選択決定
+            if (Gamepad.all[playerNum].buttonEast.wasPressedThisFrame)
+            {
+                //SoundManager.Instance.Play(SoundManager.SoundID.Decide01, 0.8f);
+                DecideState = _State;
+                var index = (int)_State;
+                _ParList[index].GetComponent<RectTransform>().localScale = new Vector3(2, 2, 2);
+                // Debug.Log(DecideState);
+            }
+        }
+        else
+        {
+            // ×を押すと選択解除
+            if (Gamepad.all[playerNum].buttonSouth.wasPressedThisFrame)
+            {
+                //SoundManager.Instance.Play(SoundManager.SoundID.Cancel, 0.5f);
+                DecideState = StateEnum.None;
+                var index = (int)_State;
+                _ParList[index].GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+            }
+        }
+    }
+
+    private void ChangeState(StateEnum next)
+    {
+        if (next == StateEnum.None) return;
+        var prev = _State;
+        _State = next;
+        // Log.Info(GetType(), "ChangeState {0} -> {1}", prev, next);
+        switch (_State)
+        {
+            case StateEnum.Start:
+                ChangeActive((int)StateEnum.Start);
+                break;
+            case StateEnum.Setting:
+                ChangeActive((int)StateEnum.Setting);
+                break;
+            case StateEnum.HomePage:
+                ChangeActive((int)StateEnum.HomePage);
+                break;
+            case StateEnum.Exit:
+                ChangeActive((int)StateEnum.Exit);
+                break;
+        }
+    }
+
+    private void ChangeActive(int nextId)
+    {
+        if (_ParList == null || _ParList.Count <= nextId || nextId == -1) return;
+        foreach (GameObject obj in _ParList)
+        {
+            obj.SetActive(false);
+        }
+        if (_ParList[nextId] == null) return;
+        _ParList[nextId].SetActive(true);
+    }
+
+    public void DeSelect()
+    {
+        DecideState = StateEnum.None;
+        var index = (int)_State;
+        _ParList[index].GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+    }
+}
---
+++ GamePit2024/Assets/Matsunaga/Script/Utility/TitletoGame.cs
@@ -0,0 +1,131 @@
+﻿using Cysharp.Threading.Tasks;
+using Game.Loader;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class TitletoGame : MonoBehaviour
+{
+    [SerializeField] private TitleSelectUI _FirstSelect;
+    [SerializeField] private TitleSelectUI _SecondSelect;
+    [SerializeField] private TitleSelectUI _ThirdSelect;
+
+    private void Awake()
+    {
+    }
+
+    private void Start()
+    {
+
+    }
+    void Update()
+    {
+        switch (Gamepad.all.Count)
+        {
+            case 0:
+                break;
+            case 1:
+                ChoiceOne();
+                break;
+            case 2:
+                ChoiceTwo();
+                break;
+            case 3:
+                ChoiceThree();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void ChoiceOne()
+    {
+        // どちらかが未選択なら処理しない
+        if (_FirstSelect.DecideState == TitleSelectUI.StateEnum.None) return;
+
+        if (_FirstSelect.DecideState == TitleSelectUI.StateEnum.Exit)
+        {
+            StartCoroutine("ExitGame");
+        }
+        else
+        {
+            var sceneIndex = (int)_FirstSelect.DecideState;
+            SceneLoader.Instance.GoToStage();
+            //SceneId nextScene = (SceneId)Enum.ToObject(typeof(SceneId), sceneIndex + 1);
+            //SceneTransferManager.Instance.Load(nextScene);
+        }
+    }
+
+    void ChoiceTwo() {
+        // どちらかが未選択なら処理しない
+        if (_FirstSelect.DecideState == TitleSelectUI.StateEnum.None || _SecondSelect.DecideState == TitleSelectUI.StateEnum.None) return;
+        // 二人の選択が異なる場合
+        if (_FirstSelect.DecideState != _SecondSelect.DecideState)
+        {
+            // 二人の選択を解除
+            _FirstSelect.DeSelect();
+            _SecondSelect.DeSelect();
+            return;
+        }
+        if (_FirstSelect.DecideState == _SecondSelect.DecideState)
+        {
+            //SoundManager.Instance.Stop();
+            //SoundManager.Instance.Play(SoundManager.SoundID.Decide02, 0.8f);
+            if (_FirstSelect.DecideState == TitleSelectUI.StateEnum.Exit)
+            {
+                StartCoroutine("ExitGame");
+            }
+            else
+            {
+                var sceneIndex = (int)_FirstSelect.DecideState;
+                SceneLoader.Instance.GoToStage();
+                //SceneId nextScene = (SceneId)Enum.ToObject(typeof(SceneId), sceneIndex + 1);
+                //SceneTransferManager.Instance.Load(nextScene);
+            }
+        }
+    }
+
+    void ChoiceThree()
+    {
+        // どちらかが未選択なら処理しない
+        if (_FirstSelect.DecideState == TitleSelectUI.StateEnum.None || _SecondSelect.DecideState == TitleSelectUI.StateEnum.None || _ThirdSelect.DecideState == TitleSelectUI.StateEnum.None) return;
+        // 二人の選択が異なる場合
+
+        if (_FirstSelect.DecideState == _SecondSelect.DecideState && _SecondSelect.DecideState == _ThirdSelect.DecideState)
+        {
+            //SoundManager.Instance.Stop();
+            //SoundManager.Instance.Play(SoundManager.SoundID.Decide02, 0.8f);
+            if (_FirstSelect.DecideState == TitleSelectUI.StateEnum.Exit)
+            {
+                StartCoroutine("ExitGame");
+            }
+            else
+            {
+                var sceneIndex = (int)_FirstSelect.DecideState;
+                SceneLoader.Instance.GoToStage();
+                //SceneId nextScene = (SceneId)Enum.ToObject(typeof(SceneId), sceneIndex + 1);
+                //SceneTransferManager.Instance.Load(nextScene);
+            }
+        } else
+        {
+            // 二人の選択を解除
+            _FirstSelect.DeSelect();
+            _SecondSelect.DeSelect();
+            _ThirdSelect.DeSelect();
+            return;
+        }
+    }
+
+    IEnumerator ExitGame()
+    {
+        yield return new WaitForSeconds(1.0f);
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;//ゲームプレイ終了
+        #else
+                Application.Quit();//ゲームプレイ終了
+        #endif
+    }
+}
